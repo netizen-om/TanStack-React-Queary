7cf346d17ade3a196dd8204e37c354c1738f58dc
netizen-om <borisagarom@gmail.com>
2025-03-01 17:16:26 +0530

    useMutation Hook complete

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,15 +1,71 @@
-import { useQuery } from '@tanstack/react-query'
+import { QueryClient, useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import React from 'react'
-import { fetchPosts } from '../api/api'
+import { addPost, fetchPosts, fetchTags } from '../api/api'
 
 function PostList() {
+
+    const quearyClient = useQueryClient()
 
     const {data:postData ,isError, error , isLoading} = useQuery({
         queryKey : ["posts"],
         queryFn : fetchPosts
     });
 
+    const { mutate, isError: isPostError, isPending, reset } = useMutation({
+        mutationFn: addPost,
+        onMutate : () => {
+            return {id : 1}
+        }
+        onSuccess : (data, variable, context) => {
+            quearyClient.invalidateQueries({
+                queryKey : ["posts"]
+            })
+        }
+    })
+
+    const { data:tagsData } = useQuery({
+        queryKey : ["tags"],
+        queryFn : fetchTags
+    })
+
+    const HandleSubmit = async(e) => {
+        e.preventDefault()
+        
+        const fromData = new FormData(e.target)
+        const title = fromData.get("title")
+        const tags = Array.from(fromData.keys("tags"))?.filter(
+            (key) => fromData.get(key) === "on"
+        )
+
+        if(!title || !tags) return
+
+        mutate({id: postData.length+1, title, tags})
+
+        e.target.reset
+    }
+
   return <div className='container'>
+
+    <form onSubmit={HandleSubmit}>
+        <input 
+        type="text" 
+        placeholder='Enter your post...'
+        className='postbox'
+        name='title'
+        />
+
+        <div className='tags'>
+            {tagsData?.map((tag) => {
+                return (
+                    <div key={tag}>
+                        <input type="checkbox" id={tag} name={tag}/>
+                        <label htmlFor={tag}>{tag}</label>
+                    </div>
+                )
+            })}
+        </div>
+        <button>Post</button>
+    </form>
 
     {isLoading && <p>Loading.......</p>}
         {isError && <p>{error?.message}</p>}
